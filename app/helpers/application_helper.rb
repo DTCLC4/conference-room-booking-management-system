c0b3bbf3626b474258b76861526bd0cd137da535
dinhthi12 <thi884641@gmail.com>
2024-06-16 14:45:46 +0700

    Initial commit

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,12 +1,10 @@
 module ApplicationHelper
   def toastr_flash
-    flash.each_with_object([]) do |(type, message),
-                                   flash_message|
+    flash.each_with_object([]) do |(type, message), flash_message|
       type = "success" if type == "notice"
       type = "error" if type == "alert"
       text = content_tag(:script, nil, type: "text/javascript") do
-        sanitize("toastr.#{type}('#{message}', '', {closeButton: true,
-          progressBar: true});")
+        sanitize("toastr.#{type}('#{message}', '', {closeButton: true, progressBar: true});")
       end
       flash_message << text if message
     end.join("\n")
