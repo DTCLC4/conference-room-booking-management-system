3c740deb1fdc6e101e7a21919a50a6f268d87264
dinhthi12 <thi884641@gmail.com>
2024-09-28 17:40:34 +0700

    [Update] sidebar show toggle #10

--- client/src/components/AppContent.jsx
+++ client/src/components/AppContent.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 
 const AppContent = () => {
-  return <div>App content work!</div>
+  return <div style={{ height: '1000px' }}>App content work!</div>
 }
 
 export default React.memo(AppContent)
